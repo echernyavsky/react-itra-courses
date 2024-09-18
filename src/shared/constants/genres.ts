c524049366f60725b917991a3b84fc4705e55da2
Edward Chernyavsky <e.chernyavsky@itransition.com>
2024-09-18 15:23:13 +0200

    Added movie genres

---
+++ src/shared/constants/genres.ts
@@ -0,0 +1,13 @@
+const genres = [
+  { field: "action", label: "Action" },
+  { field: "comedy", label: "Comedy" },
+  { field: "drama", label: "Drama" },
+  { field: "fantasy", label: "Fantasy" },
+  { field: "horror", label: "Horror" },
+  { field: "mystery", label: "Mystery" },
+  { field: "romance", label: "Romance" },
+  { field: "thriller", label: "Thriller" },
+  { field: "western", label: "Western" },
+];
+
+export default genres;
